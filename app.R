31384b1fd2bfaebfcf5f3c88259ed36628aaed6b
Sami Saada <sami.saada@gmail.com>
2018-11-30 17:48:45 +0200

    Replace fluidPage with HTML template

--- app.R
+++ app.R
@@ -3,34 +3,13 @@ library(shiny)
 op_api_token <- Sys.getenv("OP_API_TOKEN")
 
 # Define UI for app that draws a histogram ----
-ui <- fluidPage(
-  
-  # App title ----
-  titlePanel("Hello Shiny!"),
-  
-  # Sidebar layout with input and output definitions ----
-  sidebarLayout(
-    
-    # Sidebar panel for inputs ----
-    sidebarPanel(
-      
-      # Input: Slider for the number of bins ----
-      sliderInput(inputId = "bins",
-                  label = "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-      
-    ),
-    
-    # Main panel for displaying outputs ----
-    mainPanel(
-      
-      # Output: Histogram ----
-      plotOutput(outputId = "distPlot")
-      
-    )
-  )
+ui <- htmlTemplate("template.html",
+  slider = sliderInput(inputId = "bins",
+                       label = "Number of bins:",
+                       min = 1,
+                       max = 50,
+                       value = 30),
+  plotpart = plotOutput(outputId = "distPlot")
 )
 
 # Define server logic required to draw a histogram ----
