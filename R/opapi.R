b3defb686b0b4bf09ae55d6619729c4b5a8c3dcc
Jetro Anttonen <jetro.a@gmail.com>
2018-12-01 13:22:20 +0200

    "OP_API_TOKEN" changed to "OP_API_KEY"

--- R/opapi.R
+++ R/opapi.R
@@ -6,7 +6,7 @@ setClass("OPAPI", slots=list(url="character",
 
 OPAPI <- function() {
   api_url <- "https://sandbox.apis.op-palvelut.fi/v1"
-  api_key <- Sys.getenv("OP_API_TOKEN")
+  api_key <- Sys.getenv("OP_API_KEY")
   api_token <- "b6910384440ce06f495976f96a162e2ab1bafbb4"
   new("OPAPI", url=api_url, key=api_key, token=api_token)
 }
